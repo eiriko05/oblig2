e7e8ac3c2ae4a200b546df695d51d13a3a9125b5
Eirik <eisol3274@usn.no>
2025-10-20 13:39:30 +0200

    s

--- 0102.php
+++ 0102.php
@@ -5,7 +5,7 @@
 */
   include("db-tilkobling.php");  /* tilkobling til database-serveren utf�rt og valg av database foretatt */
 
-  $sqlSetning="SELECT * FROM Klassenavn;";
+  $sqlSetning="SELECT * FROM Klassekode;";
   
   $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
     /* SQL-setning sendt til database-serveren */
