c87787cb8cdc42a5b111751312694d28b622c86d
Eirik <eisol3274@usn.no>
2025-10-20 13:21:57 +0200

    s

--- 0101.php
+++ 0101.php
@@ -31,8 +31,8 @@
           include("db-tilkobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
 
           $sqlSetning="SELECT * FROM Klassekode WHERE Klassenavn='$Klassenavn';";
-          $sqlResultat=mysql_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
-          $antallRader=mysql_num_rows($sqlResultat); 
+          $sqlResultat=MySQL_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
+          $antallRader=MySQL_num_rows($sqlResultat); 
 
           if ($antallRader!=0)  /* Klassekode er registrert fra før */
             {
