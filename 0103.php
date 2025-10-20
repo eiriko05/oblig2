57ce95f09a30eff542636d64f603c1f4c3e492a6
Eirik <eisol3274@usn.no>
2025-10-20 15:20:45 +0200

    s

--- 0103.php
+++ 0103.php
@@ -10,39 +10,39 @@
 
 <h3>Slett Klassenavn</h3>
 
-<form method="post" action="" id="slettKlassekodeSkjema" name="slettKlassekodeSkjema" onSubmit="return bekreft()">
-  Klassenavn <input type="text" id="Klassekode" name="Klassekode" required /> <br/>
-  <input type="submit" value="Slett Klassenavn" name="slettKlassenavnKnapp" id="slettKlassenavnKnapp" /> 
+<form method="post" action="" id="slettklassekodeSkjema" name="slettklassekodeSkjema" onSubmit="return bekreft()">
+  Klassenavn <input type="text" id="klassekode" name="klassekode" required /> <br/>
+  <input type="submit" value="Slett klassenavn" name="slettklassenavnKnapp" id="slettklassenavnKnapp" /> 
 </form>
 
 <?php
-  if (isset($_POST ["slettKlassekodeKnapp"]))
+  if (isset($_POST ["slettKlasseKnapp"]))
     {	
-      $Klassenavn=$_POST ["Klassenavn"];
+      $Klassenavn=$_POST ["klassenavn"];
 	  
 	  if (!$Klassenavn)
         {
-          print ("Klassenavn m&aring; fylles ut");
+          print ("klassenavn m&aring; fylles ut");
         }
       else
         {
           include("db-tilkobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
 
-          $sqlSetning="SELECT * FROM Klassenavn WHERE Klassekode='$Klassenavn';";
+          $sqlSetning="SELECT * FROM klassenavn WHERE klassekode='$klassenavn';";
           $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
           $antallRader=mysqli_num_rows($sqlResultat); 
 
           if ($antallRader==0)  /* Klassekode er ikke registrert */
             {
-              print ("Klassenavn finnes ikke");
+              print ("klassenavn finnes ikke");
             }
           else
             {	  
-              $sqlSetning="DELETE FROM Klassenavn WHERE Klassekode='$Klassenavn';";
+              $sqlSetning="DELETE FROM klassenavn WHERE klassekode='$klassenavn';";
               mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
                 /* SQL-setning sendt til database-serveren */
 		
-              print ("F&oslash;lgende Klassekode er n&aring; slettet: $Klassenavn <br />");
+              print ("F&oslash;lgende Klasse er n&aring; slettet: $klassenavn <br />");
             }
         }
     }
