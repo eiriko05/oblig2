5881bc4770347728f8ed340a03b256ab3b094e53
Eirik <eisol3274@usn.no>
2025-10-20 13:25:51 +0200

    s

--- 0101.php
+++ 0101.php
@@ -40,7 +40,7 @@
             }
           else
             {
-              $sqlSetning="INSERT INTO poststed VALUES('$Klassenavn','$Klassekode');";
+              $sqlSetning="INSERT INTO Klassekode VALUES('$Klassenavn','$Klassekode');";
               mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; registrere data i databasen");
                 /* SQL-setning sendt til database-serveren */
 
