e3df8845989b504b7719a6984087eb1596281db0
Eirik <eisol3274@usn.no>
2025-10-22 14:59:07 +0200

    s

--- 0101.php
+++ 0101.php
@@ -1,67 +1,65 @@
 <?php  
-/* registrer-Student
-   Programmet lager et skjema for å registrere en Student
-   Studenten knyttes til en eksisterende klasse via en dynamisk listeboks
+/* registrer-Klasse
+   Programmet lager et skjema for å registrere en Klasse
+   Klassenavn og studiumkode skrives inn manuelt,
+   mens klassekode velges fra en dynamisk listeboks
 */
 ?>
 
-<h3>Registrer student</h3>
+<h3>Registrer klasse</h3>
 
 <?php
 include("db-tilkobling.php");  // kobler til databasen
 
-// hent alle klasser dynamisk
-$sqlSetning = "SELECT klassekode, klassenavn FROM Klasse ORDER BY klassekode;";
-$sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig å hente klasser");
+// hent alle tilgjengelige klassekoder dynamisk
+$sqlSetning = "SELECT klassekode FROM Klasse ORDER BY klassekode;";
+$sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig å hente klassekoder");
 ?>
 
-<form method="post" action="" id="registrerStudentSkjema" name="registrerStudentSkjema">
-  Brukernavn: <input type="text" id="brukernavn" name="brukernavn" required /> <br/>
-  Fornavn: <input type="text" id="fornavn" name="fornavn" required /> <br/>
-  Etternavn: <input type="text" id="etternavn" name="etternavn" required /> <br/>
+<form method="post" action="" id="registrerKlasseSkjema" name="registrerKlasseSkjema">
+  Klassenavn: <input type="text" id="klassenavn" name="klassenavn" required /> <br/>
+  Studiumkode: <input type="text" id="studiumkode" name="studiumkode" required /> <br/>
 
-  <label for="klassekode">Velg klasse:</label>
+  <label for="klassekode">Velg klassekode:</label>
   <select id="klassekode" name="klassekode" required>
-    <option value="">-- Velg klasse --</option>
+    <option value="">-- Velg klassekode --</option>
     <?php
     while ($rad = mysqli_fetch_assoc($sqlResultat)) {
         $kode = $rad['klassekode'];
-        $navn = $rad['klassenavn'];
-        echo "<option value='$kode'>$kode – $navn</option>";
+        echo "<option value='$kode'>$kode</option>";
     }
     ?>
   </select>
   <br/>
 
-  <input type="submit" value="Registrer Student" id="registrerStudentKnapp" name="registrerStudentKnapp" /> 
+  <input type="submit" value="Registrer Klasse" id="registrerKlasseKnapp" name="registrerKlasseKnapp" /> 
   <input type="reset" value="Nullstill" id="nullstill" name="nullstill" /> <br />
 </form>
 
 <?php  
-if (isset($_POST["registrerStudentKnapp"])) {
-    $brukernavn = $_POST["brukernavn"];
-    $fornavn = $_POST["fornavn"];
-    $etternavn = $_POST["etternavn"];
+if (isset($_POST["registrerKlasseKnapp"])) {
+    $klassenavn = $_POST["klassenavn"];
+    $studiumkode = $_POST["studiumkode"];
     $klassekode = $_POST["klassekode"];
 
-    if (!$brukernavn || !$fornavn || !$etternavn || !$klassekode) {
-        print("Alle felt må fylles ut");
+    if (!$klassekode || !$klassenavn || !$studiumkode) {
+        print("Både klassekode, klassenavn og studiumkode må fylles ut");
     } else {
-        // sjekk om studenten finnes fra før
-        $sqlSetning = "SELECT * FROM Student WHERE brukernavn='$brukernavn';";
+        // sjekk om klassen finnes fra før
+        $sqlSetning = "SELECT * FROM Klasse WHERE klassekode='$klassekode';";
         $sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig å hente data fra databasen");
         $antallRader = mysqli_num_rows($sqlResultat);
 
         if ($antallRader != 0) {
-            print("Student med brukernavn $brukernavn finnes allerede");
+            print("Klassekode er registrert fra før");
         } else {
-            // registrer ny student
-            $sqlSetning = "INSERT INTO Student (brukernavn, fornavn, etternavn, klassekode) 
-                           VALUES ('$brukernavn', '$fornavn', '$etternavn', '$klassekode');";
+            // registrer ny klasse
+            $sqlSetning = "INSERT INTO Klasse (klassenavn, studiumkode, klassekode) 
+                           VALUES ('$klassenavn', '$studiumkode', '$klassekode');";
             mysqli_query($db, $sqlSetning) or die("Ikke mulig å registrere data i databasen");
 
-            print("Student $fornavn $etternavn ($brukernavn) er nå registrert i klasse $klassekode");
-        } 
+            print("Følgende klasse er nå registrert: $klassenavn ($klassekode) i studium $studiumkode");
+        }
     }
 }
 ?>
