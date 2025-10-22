c60cc0f11359e0684c68fd5e2f97e41f823bbc12
Eirik <eisol3274@usn.no>
2025-10-22 17:54:53 +0200

    s

--- 0101.php
+++ 0101.php
@@ -8,15 +8,15 @@
 <h3>Registrer klasse </h3>
 
 <form method="post" action="" id="registrerklasseSkjema" name="registrerklasseSkjema">
-klassenavn <input type="text" id="klassenavn" name="klassenavn" required /> <br/>
+ klassenavn <input type="text" id="klassenavn" name="klassenavn" required /> <br/>
 studiumkode <input type="text" id="studiumkode" name="studiumkode" required /> <br/>
-klassekode <input type="text" id="klassekode" name="klassekode" required /> <br/>
-  <input type="submit" value="Registrer klassekode" id="registrerKlassekodeKnapp" name="registrerKlassekodeKnapp" />
+ klassekode <input type="text" id="klassekode" name="klassekode" required /> <br/>
+  <input type="submit" value="Registrer klasse" id="registrerKlasseKnapp" name="registrerKlasseKnapp" />
   <input type="reset" value="Nullstill" id="nullstill" name="nullstill" /> <br />
 </form>
 
 <?php
-  if (isset($_POST ["registrerKlassekodeKnapp"]))
+  if (isset($_POST ["registrerKlasseKnapp"]))
     {
       $klassenavn=$_POST ["klassenavn"];
       $studiumkode=$_POST ["studiumkode"];
