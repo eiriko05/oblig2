20979d70d812926e157a8b8d5f1adcc1f77b9e49
Eirik <eisol3274@usn.no>
2025-10-22 12:23:50 +0200

    s

--- 0104.php
+++ 0104.php
@@ -1,41 +1,52 @@
-<?php  /* registrer-Student */ ?>
-<h3>Registrer Student</h3>
+<?php  /* registrer-Student */
+/*
+/*  Programmet lager et html-skjema for å registrere en Student
+/*  Programmet registrerer data (Brukernavn Fornavn Etternavn og klassekode) i databasen
+*/
+?> 
+
+<h3>Registrer Student </h3>
 
 <form method="post" action="" id="registrerStudentSkjema" name="registrerStudentSkjema">
-  Fornavn: <input type="text" id="fornavn" name="fornavn" required /> <br/>
-  Etternavn: <input type="text" id="etternavn" name="etternavn" required /> <br/>
-  Brukernavn: <input type="text" id="brukernavn" name="brukernavn" required /> <br/>
-  Klassekode: <input type="text" id="klassekode" name="klassekode" required /> <br/>
-  <input type="submit" value="Registrer Student" id="registrerStudentKnapp" name="registrerStudentKnapp" /> 
+  Fornavn <input type="text" id="fornavn" name="fornavn" required/> <br/>
+  Etternavn <input type="text" id="etternavn" name="etternavn" required/> <br/>
+  Brukernavn <input type="text" id="brukernavn" name="brukernavn" required /> <br/>
+  Klassekode <input type="text" id="Klassekode" name="Klassekode" required /> <br/>
+  <input type="submit" value="Registrer Student" id="registrerStudentkodeKnapp" name="registrerStudentkodeKnapp" /> 
   <input type="reset" value="Nullstill" id="nullstill" name="nullstill" /> <br />
 </form>
 
-<?php
-if (isset($_POST["registrerStudentKnapp"])) {
-    $klassekode = $_POST["klassekode"];
-    $brukernavn = $_POST["brukernavn"];
-    $etternavn  = $_POST["etternavn"];
-    $fornavn    = $_POST["fornavn"];
+<?php 
+  if (isset($_POST ["registrerStudentKnapp"]))
+    {
+      $Klassekode=$_POST ["Klassekode"];
+      $brukernavn=$_POST ["brukernavn"];
+      $etternavn=$_POST ["etternavn"];
+      $fornavn=$_POST ["fornavn"];
+      if (!$Klassekode || !$brukernavn || !$etternavn || !$fornavn )
+        {
+          print ("B&aring;de Klassekode  brukernavn etternavn og fornavn m&aring; fylles ut");
+        }
+      else
+        {
+          include("db-tilkobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
 
-    if (!$klassekode || !$brukernavn || !$etternavn || !$fornavn) {
-        print("Både klassekode, brukernavn, etternavn og fornavn må fylles ut");
-    } else {
-        include("db-tilkobling.php");  // kobler til databasen
+          $sqlSetning="SELECT * FROM brukernavn WHERE fornavn and etternavn='$brukernavn';";
+          $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
+          $antallRader=mysqli_num_rows($sqlResultat); 
 
-        // sjekk om brukernavn allerede finnes
-        $sqlSetning = "SELECT * FROM Student WHERE brukernavn='$brukernavn';";
-        $sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig å hente data fra databasen");
-        $antallRader = mysqli_num_rows($sqlResultat);
+          if ($antallRader!=0)  /* Student er registrert fra før */
+            {
+              print ("Student er registrert fra f&oslashr");
+            }
+          else
+            {
+              $sqlSetning="INSERT INTO Student VALUES('$Klassekode','$brukernavn','$fornavn','$etternavn');";
+              mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; registrere data i databasen");
+                /* SQL-setning sendt til database-serveren */
 
-        if ($antallRader != 0) {
-            print("Student er registrert fra før");
-        } else {
-            $sqlSetning = "INSERT INTO Student (klassekode, brukernavn, fornavn, etternavn)
-                           VALUES ('$klassekode','$brukernavn','$fornavn','$etternavn')";
-            mysqli_query($db, $sqlSetning) or die("Ikke mulig å registrere data i databasen");
-
-            print("Følgende student er nå registrert: $fornavn $etternavn ($brukernavn) i klasse $klassekode");
+              print ("F&oslash;lgende Student er n&aring; registrert: $fornavn $Klassekode $etternavn $brukernavn"); 
+            }
         }
     }
-}
-?>
+?> 
