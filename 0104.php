a377d47994737e9f0336040db102c290b65ad9a0
Eirik <eisol3274@usn.no>
2025-10-22 12:32:40 +0200

    s

--- 0104.php
+++ 0104.php
@@ -1,15 +1,15 @@
 <?php  /* registrer-Student */
 /*
 /*  Programmet lager et html-skjema for å registrere en Student
-/*  Programmet registrerer data (Brukernavn, Fornavn, Etternavn og Klassekode) i databasen
+/*  Programmet registrerer data (Brukernavn Fornavn Etternavn og klassekode) i databasen
 */
 ?>
 
-<h3>Registrer Student</h3>
+<h3>Registrer Student </h3>
 
 <form method="post" action="" id="registrerStudentSkjema" name="registrerStudentSkjema">
-  Fornavn <input type="text" id="fornavn" name="fornavn" required /> <br/>
-  Etternavn <input type="text" id="etternavn" name="etternavn" required /> <br/>
+  Fornavn <input type="text" id="fornavn" name="fornavn" required/> <br/>
+  Etternavn <input type="text" id="etternavn" name="etternavn" required/> <br/>
   Brukernavn <input type="text" id="brukernavn" name="brukernavn" required /> <br/>
   Klassekode <input type="text" id="Klassekode" name="Klassekode" required /> <br/>
   <input type="submit" value="Registrer Student" id="registrerStudentKnapp" name="registrerStudentKnapp" />
@@ -18,35 +18,12 @@
 
 <?php
 if (isset($_POST["registrerStudentKnapp"])) {
-  include("db-tilkobling.php");  // Tilkobling til database
-
-  $Klassekode = mysqli_real_escape_string($db, $_POST["Klassekode"]);
-  $brukernavn = mysqli_real_escape_string($db, $_POST["brukernavn"]);
-  $etternavn = mysqli_real_escape_string($db, $_POST["etternavn"]);
-  $fornavn = mysqli_real_escape_string($db, $_POST["fornavn"]);
+  $Klassekode = $_POST["Klassekode"];
+  $brukernavn = $_POST["brukernavn"];
+  $etternavn = $_POST["etternavn"];
+  $fornavn = $_POST["fornavn"];
 
   if (!$Klassekode || !$brukernavn || !$etternavn || !$fornavn) {
-    echo "Både Klassekode, brukernavn, etternavn og fornavn må fylles ut.";
+    print ("B&aring;de Klassekode, brukernavn, etternavn og fornavn m&aring; fylles ut");
   } else {
-    // Sjekk om brukernavn allerede finnes
-    $sqlSetning = "SELECT * FROM Student WHERE brukernavn = '$brukernavn';";
-    $sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig å hente data fra databasen");
-    $antallRader = mysqli_num_rows($sqlResultat);
-
-    if ($antallRader != 0) {
-      echo "Student er registrert fra før.";
-    } else {
-      // Sett inn ny Student
-      $sqlSetning = "INSERT INTO Student (klassekode, brukernavn, fornavn, etternavn)
-                     VALUES ('$Klassekode', '$brukernavn', '$fornavn', '$etternavn');";
-      mysqli_query($db, $sqlSetning) or die("Ikke mulig å registrere data i databasen");
-
-      echo "Følgende student er nå registrert: " .
-           htmlspecialchars($fornavn) . " " .
-           htmlspecialchars($etternavn) . " (" .
-           htmlspecialchars($brukernavn) . "), klassekode: " .
-           htmlspecialchars($Klassekode);
-    }
-  }
-}
-?>
+    include("db-tilkobling.php
