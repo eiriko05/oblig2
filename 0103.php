ddb84eba0bda00938ac00fc566bae5ad18e13b4d
Eirik <eisol3274@usn.no>
2025-10-22 14:13:26 +0200

    s

--- 0103.php
+++ 0103.php
@@ -15,12 +15,15 @@
 </form>
 
 <?php
-if (isset($_POST["slettKlasseKnapp"])) {
+if (isset($_POST["slettKlasseKnapp"])) 
+{
     $klassekode = $_POST["klassekode"];
 
-    if (!$klassekode) {
+    if (!$klassekode) 
+    {
         print("Klasse må fylles ut");
-    } else {
+    } else 
+    {
         include("db-tilkobling.php");  // kobler til databasen
 
         // sjekk om Klassen finnes
@@ -28,15 +31,27 @@ if (isset($_POST["slettKlasseKnapp"])) {
         $sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig å hente data fra databasen");
         $antallRader = mysqli_num_rows($sqlResultat);
 
-        if ($antallRader == 0) {
-            print("Klasse finnes ikke");
-        } else {
-            // slett Klassen
-            $sqlSetning = "DELETE FROM Klasse WHERE klassekode='$klassekode';";
-            mysqli_query($db, $sqlSetning) or die("Ikke mulig å slette data i databasen");
+       if ($antallRader == 0) 
+ {
+    print("Klasse finnes ikke");
+ } else {
+    // Sjekk om det finnes studenter i denne klassen
+    $sqlSetning = "SELECT COUNT(*) AS antall FROM Student WHERE klassekode='$klassekode';";
+    $resultat = mysqli_query($db, $sqlSetning);
+    $row = mysqli_fetch_assoc($resultat);
 
-            print("Følgende klasse er nå slettet: $klassekode <br />");
-        }
-    }
+    if ($row['antall'] > 0) 
+{
+        print("Kan ikke slette klassen fordi den har registrerte studenter.");
+} else 
+{
+        // Slett klassen hvis ingen studenter er registrert
+        $sqlSetning = "DELETE FROM Klasse WHERE klassekode='$klassekode';";
+        mysqli_query($db, $sqlSetning) or die("Ikke mulig å slette klassen");
+        print("Klassen $klassekode er slettet.");
+    
+}
+ }
+    } 
 }
 ?>
