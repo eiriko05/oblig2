29e241b2aa98cf067d90e541c3f5b2f39ec715ca
Eirik <eisol3274@usn.no>
2025-10-22 14:10:14 +0200

    s

--- 0106.php
+++ 0106.php
@@ -31,17 +31,28 @@ if (isset($_POST["slettStudentKnapp"]))
         $sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig å hente data fra databasen");
         $antallRader = mysqli_num_rows($sqlResultat);
 
-        if ($antallRader == 0) 
-        {
-            print("Student finnes ikke");
-        } else 
-        {
-            // slett Klassen
-            $sqlSetning = "DELETE FROM Student WHERE brukernavn='$brukernavn';";
-            mysqli_query($db, $sqlSetning) or die("Ikke mulig å slette data i databasen");
 
-            print("Følgende Student er nå slettet: $brukernavn <br />");
-        }
+       if ($antallRader == 0) 
+ {
+    print("Klasse finnes ikke");
+ } else 
+ {
+    // Sjekk om det finnes studenter i denne klassen
+    $sqlSetning = "SELECT COUNT(*) AS antall FROM Student WHERE klassekode='$klassekode';";
+    $resultat = mysqli_query($db, $sqlSetning);
+    $row = mysqli_fetch_assoc($resultat);
+
+    if ($row['antall'] > 0) 
+    {
+        print("Kan ikke slette klassen fordi den har registrerte studenter.");
+    } else 
+    {
+        // Slett klassen hvis ingen studenter er registrert
+        $sqlSetning = "DELETE FROM Klasse WHERE klassekode='$klassekode';";
+        mysqli_query($db, $sqlSetning) or die("Ikke mulig å slette klassen");
+        print("Klassen $klassekode er slettet.");
+    }
+ } 
 }
- }
+    }
 ?>
