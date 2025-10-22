7f4570dcd2ae20beb7393368effc967caa01c763
Eirik <eisol3274@usn.no>
2025-10-22 12:53:03 +0200

    s

--- 0104.php
+++ 0104.php
@@ -11,30 +11,30 @@
   Fornavn <input type="text" id="fornavn" name="fornavn" required/> <br/>
   Etternavn <input type="text" id="etternavn" name="etternavn" required/> <br/>
   Brukernavn <input type="text" id="brukernavn" name="brukernavn" required /> <br/>
-  Klassekode <input type="text" id="Klassekode" name="Klassekode" required /> <br/>
+  Klassekode <input type="text" id="klassekode" name="klassekode" required /> <br/>
   <input type="submit" value="Registrer Student" id="registrerStudentKnapp" name="registrerStudentKnapp" />
   <input type="reset" value="Nullstill" id="nullstill" name="nullstill" /> <br />
 </form>
 
 <?php
 if (isset($_POST["registrerStudentKnapp"])) {
-  $Klassekode = $_POST["Klassekode"];
+  $klassekode = $_POST["klassekode"];
   $brukernavn = $_POST["brukernavn"];
   $etternavn = $_POST["etternavn"];
   $fornavn = $_POST["fornavn"];
 
-  if (!$Klassekode || !$brukernavn || !$etternavn || !$fornavn) {
+  if (!$klassekode || !$brukernavn || !$etternavn || !$fornavn) {
     print("B&aring;de Klassekode, brukernavn, etternavn og fornavn m&aring; fylles ut");
   } else {
     include("db-tilkobling.php"); // må definere $kobling her
 
-    $sql = "INSERT INTO Student (brukernavn, fornavn, etternavn, klassekode)
-            VALUES ('$brukernavn', '$fornavn', '$etternavn', '$Klassekode')";
+    $sql = "INSERT INTO Student (brukernavn,fornavn,etternavn,klassekode)
+            VALUES ('$brukernavn','$fornavn','$etternavn','$klassekode')";
 
     if (mysqli_query($kobling, $sql)) {
       print("Student er registrert");
     } else {
-      print("Feil ved registrering: " . mysqli_error($kobling));
+      print("Feil ved registrering:" . mysqli_error($kobling));
     }
   }
 }
